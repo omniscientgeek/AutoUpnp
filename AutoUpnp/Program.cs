8e8271e76babb7fdc29548b79ef9e71b3047f18b
Justin <justin@omniscientgeek.com>
2025-07-03 00:55:58 -0600

    Resolve issue when IP address is updated

--- AutoUpnp/Program.cs
+++ AutoUpnp/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AutoUpnp;
-using Mono.Nat;
+using System;
+using System.Threading;
 
 Console.WriteLine("Hello, World!");
 
--- AutoUpnp/UpnpAutoUpdate.cs
+++ AutoUpnp/UpnpAutoUpdate.cs
@@ -1,73 +1,72 @@
-﻿using Mono.Nat;
+﻿using Open.Nat;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AutoUpnp
 {
     public class UpnpAutoUpdate
     {
-        public void Run()
+        public async void Run()
         {
-            NatUtility.DeviceFound += DeviceFound;
-            NatUtility.StartDiscovery();
+            var discovery = new NatDiscoverer();
+
+            // 5 senconds or less
+            var cts = new CancellationTokenSource(5000);
 
             while (true)
             {
                 try
                 {
-                    if (!NatUtility.IsSearching)
+                    var devices = await discovery.DiscoverDevicesAsync(PortMapper.Upnp, cts);
+
+                    foreach (var d in devices)
                     {
-                        NatUtility.StopDiscovery();
+                        await CheckAsync(d);
                     }
+
                     Thread.Sleep(60000);
-                } catch(Exception e) {
+                }
+                catch (Exception e)
+                {
                     Console.WriteLine(e.ToString());
                 }
             }
         }
 
-        private void DeviceFound(object? sender, DeviceEventArgs args)
-        {
-            INatDevice device = args.Device;
-
-            Check(device);
-        }
-
-        void Check(INatDevice device)
+        async Task CheckAsync(NatDevice device)
         {
             while (true)
             {
-                Check(device, 80, Protocol.Tcp);
-                Check(device, 443, Protocol.Tcp);
-                Check(device, 51820, Protocol.Udp);
-                Check(device, 19132, Protocol.Udp);
-                Check(device, 19133, Protocol.Udp);
-
-                //Dayz
-                Check(device, 2302, Protocol.Udp);
-                Check(device, 2303, Protocol.Udp);
-                Check(device, 2304, Protocol.Udp);
-                Check(device, 2305, Protocol.Udp);
-                Check(device, 27016, Protocol.Udp);
+                await CheckAsync(device, 80, Protocol.Tcp);
+                await CheckAsync(device, 443, Protocol.Tcp);
+                await CheckAsync(device, 51820, Protocol.Udp);
+                await CheckAsync(device, 19132, Protocol.Udp);
+                await CheckAsync(device, 19133, Protocol.Udp);
+                await CheckAsync(device, 27017, Protocol.Tcp);
 
 
                 Thread.Sleep(1000);
             }
         }
 
-        void Check(INatDevice device, int port, Protocol protocol)
+        async Task CheckAsync(NatDevice device, int port, Protocol protocol)
         {
             try
             {
-                var mapping = device.GetAllMappings().Where(p => p.PublicPort == port && p.Protocol == protocol);
+                var mapping = await device.GetSpecificMappingAsync(protocol, port);
 
-                if (mapping != null && mapping.Any())
-                    return;
+                if (mapping != null && !IsLocalAddress(mapping.PrivateIP))
+                {
+                    await device.DeletePortMapAsync(new Mapping(protocol, port, port));
+                }
 
-                var newMapping = device.CreatePortMap(new Mapping(protocol, port, port, 900, "Justin Computer Webserver"));
+                await device.CreatePortMapAsync(new Mapping(protocol, port, port, 900, "Justin Computer Webserver"));
             }
             catch (Exception ex)
             {
@@ -76,6 +75,18 @@ namespace AutoUpnp
                     Console.WriteLine(ex);
                 }
             }
+        }
+
+        bool IsLocalAddress(IPAddress privateIP)
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (privateIP.Equals(ip))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
